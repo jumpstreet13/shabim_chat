d38eb3678dd95dbbaa05bf96458810b53a5def19
abakarmagomedov <amagomedov@smedialink.com>
2018-02-16 11:18:30 +0300

    button click listener is added

--- app/src/main/java/com/example/abakarmagomedov/shabimchat/MainActivity.java
+++ app/src/main/java/com/example/abakarmagomedov/shabimchat/MainActivity.java
@@ -2,11 +2,23 @@ package com.example.abakarmagomedov.shabimchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Button: ", "button clicked");
+            }
+        });
     }
 }
