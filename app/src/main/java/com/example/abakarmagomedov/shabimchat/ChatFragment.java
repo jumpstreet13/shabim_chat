73ef066918d476d3ad172b7db9afec1f77e72430
abakarmagomedov <amagomedov@smedialink.com>
2018-02-22 22:13:45 +0300

    navigation drawer is added

---
+++ app/src/main/java/com/example/abakarmagomedov/shabimchat/ChatFragment.java
@@ -0,0 +1,89 @@
+package com.example.abakarmagomedov.shabimchat;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.abakarmagomedov.shabimchat.Entity.ChatEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatFragment extends Fragment implements ChatAdapter.ChatClickListener {
+
+    private RecyclerView recyclerView;
+    private ChatAdapter chatAdapter;
+    private List<ChatEntity> chats;
+    private ChatClickedListener listener;
+
+    public interface ChatClickedListener {
+        void chatRoomClicked(int chatId);
+    }
+
+    public ChatFragment() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+    public static ChatFragment newInstance() {
+        ChatFragment fragment = new ChatFragment();
+        //Bundle args = new Bundle();
+        //args.putString(ARG_PARAM1, param1);
+        //args.putString(ARG_PARAM2, param2);
+        //fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof MainActivity) {
+            listener = (ChatClickedListener) context;
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        recyclerView = view.findViewById(R.id.chats_recycler_view);
+        chats = new ArrayList<>();
+        ChatEntity chatEntity = new ChatEntity("Abakar Magomedov", "Hi Abakar", 24325435L);
+        ChatEntity chatEntity2 = new ChatEntity("Abakar Magomedov", "Hi Abakar", 24325435L);
+        ChatEntity chatEntity3 = new ChatEntity("Abakar Magomedov", "Hi Abakar", 24325435L);
+        ChatEntity chatEntity4 = new ChatEntity("Abakar Magomedov", "Hi Abakar", 24325435L);
+        chats.add(chatEntity);
+        chats.add(chatEntity2);
+        chats.add(chatEntity3);
+        chats.add(chatEntity4);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        chatAdapter = new ChatAdapter(chats, this);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                layoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(chatAdapter);
+        return view;
+    }
+
+    @Override
+    public void chatClicked(int chatId) {
+        if (listener != null) {
+            listener.chatRoomClicked(chatId);
+        }
+    }
+
+}
--- app/src/main/java/com/example/abakarmagomedov/shabimchat/ChatRoomFragment.java
+++ app/src/main/java/com/example/abakarmagomedov/shabimchat/ChatRoomFragment.java
@@ -1,21 +1,27 @@
 package com.example.abakarmagomedov.shabimchat;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import com.example.abakarmagomedov.shabimchat.Entity.ChatEntity;
 import com.example.abakarmagomedov.shabimchat.Entity.Message;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-public class ChatRoomActivity extends AppCompatActivity implements ChatAdapter.ChatClickListener {
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChatRoomFragment extends Fragment {
 
     public static final String CHAT_ROOM_ID = "chat_room_id";
     private int chatId;
@@ -25,18 +31,37 @@ public class ChatRoomActivity extends AppCompatActivity implements ChatAdapter.C
     private EditText messageEditText;
     private MessageAdapter messageAdapter;
 
+    public ChatRoomFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static ChatRoomFragment newInstance(int chatRoomId) {
+        ChatRoomFragment fragment = new ChatRoomFragment();
+        Bundle args = new Bundle();
+        args.putInt(CHAT_ROOM_ID, chatRoomId);
+        //args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_room);
-        chatId = getIntent().getIntExtra(CHAT_ROOM_ID, -5);
-        chatRecyclerView = findViewById(R.id.recyclerview_message_list);
-        sendMessageButton = findViewById(R.id.button_chatbox_send);
-        messageEditText = findViewById(R.id.edittext_chatbox);
+        chatId = getArguments().getInt(CHAT_ROOM_ID, -5);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat_room, container, false);
+        chatRecyclerView = view.findViewById(R.id.recyclerview_message_list);
+        sendMessageButton = view.findViewById(R.id.button_chatbox_send);
+        messageEditText = view.findViewById(R.id.edittext_chatbox);
         messages = new ArrayList<>();
         messages.add(new Message("Hello my friend", System.currentTimeMillis()));
         messageAdapter = new MessageAdapter(messages);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         layoutManager.setReverseLayout(true);
         chatRecyclerView.setLayoutManager(layoutManager);
         chatRecyclerView.setAdapter(messageAdapter);
@@ -52,10 +77,7 @@ public class ChatRoomActivity extends AppCompatActivity implements ChatAdapter.C
                 messageEditText.setText("");
             }
         });
+        return view;
     }
 
-    @Override
-    public void chatClicked(int chatId) {
-
-    }
 }
