bbc9aa7f099ba666dd5c2276695cddd5012a136a
silicium93 <36299801+silicium93@users.noreply.github.com>
2018-02-23 18:52:27 +0300

    added activity_settings added class activity_settings

---
+++ app/src/main/java/com/example/abakarmagomedov/shabimchat/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.example.abakarmagomedov.shabimchat;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Никита on 23.02.2018.
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
