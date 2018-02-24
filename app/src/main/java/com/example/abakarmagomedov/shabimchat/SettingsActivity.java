0ae55f4a5851e60fc550244d8f21f66adc3c6211
silicium93 <36299801+silicium93@users.noreply.github.com>
2018-02-24 17:34:52 +0300

    vibro now is working

--- app/src/main/java/com/example/abakarmagomedov/shabimchat/SettingsActivity.java
+++ app/src/main/java/com/example/abakarmagomedov/shabimchat/SettingsActivity.java
@@ -1,8 +1,10 @@
 package com.example.abakarmagomedov.shabimchat;
 
+import android.app.Notification;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -14,21 +16,23 @@ import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
     private Switch sound_sw, vibro_sw;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         sound_sw = findViewById(R.id.sound_sw);
         vibro_sw = findViewById(R.id.vibro_sw);
+        final Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
         sound_sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (sound_sw.isChecked()){
+                if (sound_sw.isChecked()) {
                     Toast.makeText(getApplicationContext(), "Звуковые уведомления включены", Toast.LENGTH_SHORT).show();
                     //Написать кокманду включения и отключения звука на устройстве
-                }
-                else {
+                } else {
                     Toast.makeText(getApplicationContext(), "Звуковые уведомления отключены", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -36,17 +40,15 @@ public class SettingsActivity extends AppCompatActivity {
         vibro_sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (vibro_sw.isChecked()){
+                if (vibro_sw.isChecked()) {
                     Toast.makeText(getApplicationContext(), "Вибрация включена", Toast.LENGTH_SHORT).show();
-                    //Написать кокманду включения и отключения вибрации на устройстве
-                }
-                else {
+                    vibrator.vibrate(50);
+                } else {
                     Toast.makeText(getApplicationContext(), "Вибрация отключена", Toast.LENGTH_SHORT).show();
+                    vibrator.cancel();
                 }
             }
         });
 
-
-
-         }
+    }
 }
